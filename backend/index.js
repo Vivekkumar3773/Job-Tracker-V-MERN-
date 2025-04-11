2e47a6a250333a727d1d69428100e888eb576381
Vivekkumar3773 <vivekrocky5223@gmail.com>
2025-04-11 16:49:20 +0530

    This is an Student Job Tracker Web-App MERN Stack

--- backend/index.js
+++ backend/index.js
@@ -16,26 +16,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// ✅ Correct CORS setup
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    https: '//job-tracker-v-mern.vercel.app',
+    origin: [
+        "http://localhost:5173",
+        "https://job-tracker-v-mern.vercel.app"
+    ],
     credentials: true
-}
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// api routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
+// server start
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
